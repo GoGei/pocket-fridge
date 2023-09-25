a6247cf42ca2f33a9a24cee1c3fba73e5a9bdd28
richard <rich.290401@gmail.com>
2023-09-25 14:26:55 +0200

    PF-Public-part: - Added public part login template - Added public part register template - Added public part forgot password template

--- Public/Home/urls.py
+++ Public/Home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'$', views.home_index, name='home-index'),
+    url(r'register/$', views.register, name='register'),
+    url(r'login/$', views.login, name='login'),
+    url(r'forgot-password/$', views.forgot_password, name='forgot-password'),
+    url(r'licences/$', views.licences, name='licences'),
 ]
