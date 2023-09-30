6ccfb5331cb1c3d412a886bb926254b0f9abfcea
richard <rich.290401@gmail.com>
2023-09-30 14:42:30 +0200

    PF-tests: - Add API tests

--- Api/v1/Fridge/tests/test_fridge_views.py
+++ Api/v1/Fridge/tests/test_fridge_views.py
@@ -1,10 +1,5 @@
 from django.test import TestCase
-from django.utils import timezone
-from factory import fuzzy
-
-from Api.v1.tests.base_test_case import ReadOnlyViewSetMixinTestCase, ModelViewSetTestCase
-
-from core.Fridge.models import Fridge, FridgeProduct
+from Api.v1.tests.base_test_case import ReadOnlyViewSetMixinTestCase
 from core.Fridge.factories import FridgeTypeFactory, FridgeFactory, FridgeProductFactory
 
 
@@ -15,33 +10,19 @@ class FridgeTypeTestCase(ReadOnlyViewSetMixinTestCase, TestCase):
         self.instance = FridgeTypeFactory.create()
 
 
-class FridgeTestCase(ModelViewSetTestCase, TestCase):
+class FridgeTestCase(ReadOnlyViewSetMixinTestCase, TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.base_route = 'fridge'
         self.instance = FridgeFactory.create(user=self.user)
-        self.data = {
-            'fridge_type': FridgeTypeFactory.create().id,
-            'name': fuzzy.FuzzyText(length=64).fuzz(),
-        }
-        self.model = Fridge
 
 
-class FridgeProductTestCase(ModelViewSetTestCase, TestCase):
+class FridgeProductTestCase(ReadOnlyViewSetMixinTestCase, TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.base_route = 'fridge-products'
         self.fridge = FridgeFactory.create(user=self.user)
         self.instance = FridgeProductFactory.create(user=self.user, fridge=self.fridge)
-        self.data = {
-            'name': fuzzy.FuzzyText(length=64).fuzz(),
-            'amount': fuzzy.FuzzyDecimal(1, 100).fuzz(),
-            'units': fuzzy.FuzzyChoice(dict(FridgeProduct.FridgeProductUnits.choices).keys()).fuzz(),
-            'manufacture_date': fuzzy.FuzzyDate(start_date=timezone.now().date()).fuzz(),
-            'shelf_life_date': fuzzy.FuzzyDate(start_date=timezone.now().date()).fuzz(),
-            'notes': fuzzy.FuzzyText(length=2048).fuzz(),
-        }
-        self.model = FridgeProduct
 
     def get_list_url(self, action=None, *args, **kwargs):
         action = action or self.APIActions.LIST
